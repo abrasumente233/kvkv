22680da4e3896d2ee332c983bc17e7f4e3052cd6
abrasumente <345396594@qq.com>
2022-04-24 00:42:47 +0800

    (net) written an echo server using tokio_util::codec

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::error::Error;
 
+use bytes::BytesMut;
 use tokio::net::{TcpListener, TcpStream};
+use tokio_util::codec::{Decoder, Encoder};
+use futures::{stream::StreamExt, SinkExt};
 
 mod kvstore;
 
@@ -19,7 +22,42 @@ async fn run_echo_server() -> Result<(), Box<dyn Error>> {
     }
 }
 
-async fn process_socket(mut socket: TcpStream) {
-    let (mut rx, mut tx) = socket.split();
-    tokio::io::copy(&mut rx, &mut tx).await.unwrap();
+async fn process_socket(socket: TcpStream) {
+    let codec = EchoCodec {};
+    let mut conn = codec.framed(socket);
+    while let Some(message) = conn.next().await {
+        if let Ok(message) = message {
+            println!("received: {:?}", message);
+            conn.send(message).await.unwrap();
+        }
+    }
+}
+
+struct EchoCodec;
+type Message = String;
+
+impl Decoder for EchoCodec {
+    type Item = Message;
+
+    type Error = std::io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if src.is_empty() {
+            return Ok(None);
+        }
+
+        let src = src.split();
+        let message = String::from_utf8_lossy(&src).to_string();
+
+        Ok(Some(message))
+    }
+}
+
+impl Encoder<Message> for EchoCodec {
+    type Error = std::io::Error;
+
+    fn encode(&mut self, item: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        dst.extend(item.as_bytes());
+        Ok(())
+    }
 }
