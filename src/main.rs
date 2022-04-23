ca1db5d701445ecc0e7eda6b5081968e46df9f96
abrasumente <345396594@qq.com>
2022-04-23 23:48:53 +0800

    (net) add basic echo server based on tokio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use std::error::Error;
+
+use tokio::net::{TcpListener, TcpStream};
+
 mod kvstore;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    run_echo_server().await?;
+    Ok(())
+}
+
+async fn run_echo_server() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:1337").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        process_socket(socket).await;
+    }
+}
+
+async fn process_socket(mut socket: TcpStream) {
+    let (mut rx, mut tx) = socket.split();
+    tokio::io::copy(&mut rx, &mut tx).await.unwrap();
 }
