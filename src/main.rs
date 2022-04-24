73cca3f3f238da9e66cfeea72170ad6cfa6c799e
abrasumente <345396594@qq.com>
2022-04-24 10:30:50 +0800

    (parser) add working RESP decoder

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::error::Error;
 
-use bytes::{BytesMut, Buf};
+use bytes::{Buf, BytesMut};
 use futures::stream::StreamExt;
 use memchr::memchr;
 use tokio::net::{TcpListener, TcpStream};
@@ -88,10 +88,52 @@ fn bulk_string(src: &[u8]) -> Option<(RedisValue, usize)> {
     // TODO: Use Result to indicate error.
     let (_len, pos_len) = int(src)?; // @TODO: Check length
     let (data, pos_data) = word(&src[pos_len..])?;
-    
+
     let s = String::from_utf8_lossy(&data).to_string(); // TODO: Eliminate copy
 
     Some((RedisValue::BulkString(s), pos_len + pos_data))
+}
+
+fn array(src: &[u8]) -> Option<(RedisValue, usize)> {
+    let mut total_pos = 0;
+    let (len, pos_len) = int(src)?;
+
+    // TODO: It's becoming tedious to manually update total pos,
+    // and advance buffer cursor.
+    // It would be great if we have one type that does it for us.
+    total_pos += pos_len;
+    let mut src = &src[pos_len..];
+
+    let mut array = Vec::new();
+    array.reserve(len as usize);
+
+    for _ in 0..len {
+        let (value, data_len) = parse(src)?;
+        array.push(value);
+        src = &src[data_len..]; // FIXME: Cleanup
+        total_pos += data_len;
+    }
+
+    Some((RedisValue::Array(array), total_pos))
+}
+
+fn parse(src: &[u8]) -> Option<(RedisValue, usize)> {
+    if src.len() == 0 {
+        return None;
+    }
+
+    let first_byte = src[0];
+    let remain = &src[1..];
+
+    match first_byte {
+        b'+' => simple_string(remain),
+        b'-' => error(remain),
+        b':' => integer(remain),
+        b'$' => bulk_string(remain),
+        b'*' => array(remain),
+        _ => None, // TODO: Report more concrete error.
+    }
+    .map(|(v, p)| (v, p + 1)) // Plus the first byte
 }
 
 impl Decoder for RedisCodec {
@@ -100,29 +142,12 @@ impl Decoder for RedisCodec {
     type Error = std::io::Error;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if src.is_empty() {
-            return Ok(None);
-        }
-
-        let first_byte = src[0];
-        let remain = &src[1..];
-
-        let result = match first_byte {
-            b'+' => simple_string(remain),
-            b'-' => error(remain),
-            b':' => integer(remain),
-            b'$' => bulk_string(remain),
-            _ => panic!("Unknown first byte"), // TODO: Be a nice Rust citizen, don't panic
-        };
-
-        match result {
+        match parse(src) {
             Some((value, pos)) => {
-                src.advance(pos + 1); // plus the first byte
+                src.advance(pos);
                 Ok(Some(value))
-            },
-            None =>
-                Ok(None)
-            ,
+            }
+            None => Ok(None),
         }
     }
 }
